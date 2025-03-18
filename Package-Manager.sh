8bc505e26062693fffcf787c47f73dd5ff2098f7
itvedant-devops-repo <gopal.sharma@itvedant.com>
2025-03-18 13:46:51 +0530

    Add files via upload

---
+++ Package-Manager.sh
@@ -0,0 +1,7 @@
+# Use all the commands you can use to manage the package
+# This script should:
+# 1. Check if a package is installed
+# 2. Install a package if not found
+# 3. Remove a package if the user chooses
+
+# Start your script below this line
---
+++ Process-Manager.sh
@@ -0,0 +1,7 @@
+# Use all the commands you can use to manage the process
+# This script should:
+# 1. List all running processes
+# 2. Search for a specific process
+# 3. Kill a process by its Process ID (PID)
+
+# Start your script below this line
